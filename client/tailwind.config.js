834a6242e1415f1c34f00c6c4d0217adf5f4642e
Apps247 <67941617+Apps247@users.noreply.github.com>
2024-05-22 16:13:38 +0000

    Setup

---
+++ client/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
