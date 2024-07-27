f9a3b32fd4c206dcbe1a7d0f894cc7886ae78296
Apps247 <67941617+Apps247@users.noreply.github.com>
2024-07-27 08:50:57 +0000

    Added supabase code interface for resume functions

---
+++ skillsync-client/src/supabase/Resumes.ts
@@ -0,0 +1,79 @@
+import supabase from './supabaseClient'
+import { GetUserId } from './GetUserId'
+
+
+async function AddResume(resume_file, resume_label): Promise<Boolean> {
+    if (!resume_file) { console.warn("Resume file missing"); return false; }
+
+    const user_id = await GetUserId();
+    const fileExtension = resume_file.name.split('.').pop()
+    const fileName = `${user_id}_${Math.random()}.${fileExtension}`
+
+    try {
+        const updates = {
+            id: user_id,
+            resume_label: resume_label,
+            resume_url: fileName
+        }
+
+        // First upload the avatar to supabase storage
+        const { error: upload_error } = await supabase.storage.from('resume').upload(fileName, image)
+        if (upload_error) { throw new Error(upload_error.message) }
+
+        // Then update the user profile to link to the image
+        const { error } = await supabase.from('user_resumes').insert(updates)
+        if (error) { throw error.message }
+
+        return true
+
+    } catch (error) {
+        console.warn(error)
+        return false
+    }
+}
+
+async function GetResumes(): Promise<any> {
+    const user_id = await GetUserId();
+    const { data, error } = await supabase
+        .from('user_resumes')
+        .select('id, resume_label, resume_url')
+        .eq('id', user_id)
+    if (error) { console.warn(error) }
+    return data
+}
+
+// Rturn the resume id, label, and file url
+async function GetResume(resume_id): Promise<any> {
+    const user_id = await GetUserId();
+    const { data, error } = await supabase
+        .from('user_resumes')
+        .select('id, resume_label, resume_url')
+        .eq('id', user_id)
+        .eq('resume_id', resume_id)
+    if (error) { console.warn(error) }
+    return data
+}
+
+async function DeleteResume(resume_id): Promise<Boolean> {
+    const user_id = await GetUserId();
+    const { error } = await supabase
+        .from('user_resumes')
+        .delete()
+        .eq('id', user_id)
+        .eq('resume_id', resume_id)
+    if (error) { console.warn(error) 
+        return false;
+    }
+    return true;
+}
+
+async function GetResumeCount(): Promise<Number | null> {
+    const user_id = await GetUserId();
+    const { data, error, count } = await supabase
+        .from('user_resumes')
+        .select('*', { count: 'exact' })
+        .eq('id', user_id)
+    if (error) { console.warn(error); return null;}
+    return count;
+
+export { GetResumeCount, GetResumes, GetResume, AddResume, DeleteResume }
