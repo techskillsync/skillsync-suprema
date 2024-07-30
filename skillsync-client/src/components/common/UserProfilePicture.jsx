83c0664193b90d76a63d93f8f9b09ea1def9948f
Apps247 <67941617+Apps247@users.noreply.github.com>
2024-07-30 21:42:39 +0000

    Fixed Profile Picture Bug

--- skillsync-client/src/components/common/UserProfilePicture.jsx
+++ skillsync-client/src/components/common/UserProfilePicture.jsx
@@ -1,21 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import { GetAvatar } from '../../supabase/ProfilePicture'
+import React, { useEffect, useState } from "react";
+import { GetAvatar } from "../../supabase/ProfilePicture";
+import { FaUser } from "react-icons/fa";
 
-function ProfilePicture({height, width}) {
-    const [avatarUrl, setAvatarUrl] = useState(null)
+function ProfilePicture({ height, width }) {
+  const [avatarUrl, setAvatarUrl] = useState(null);
 
-    async function fetchPfpUrl() {
-        setAvatarUrl(await GetAvatar())
-    }
+  async function fetchPfpUrl() {
+    setAvatarUrl(await GetAvatar());
+  }
 
-    useEffect(() => {
-        fetchPfpUrl()
-    }, [])
+  useEffect(() => {
+    fetchPfpUrl();
+  }, []);
 
-
-    return(
-            (avatarUrl && <img className='fade-in' src={avatarUrl}  />)
-    )
+  return avatarUrl ? (
+    <img className="fade-in" src={avatarUrl} />
+  ) : (
+    <div className="w-12 h-12">
+      <div className="rounded-full bg-gray-800 w-full h-full">
+        <FaUser className="text-gray-200 w-full h-full p-3" />
+      </div>
+    </div>
+  );
 }
 
-export default ProfilePicture
+export default ProfilePicture;
