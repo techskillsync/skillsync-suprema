3e7e16e601f2e293089c56ea51409d4f9feb2f0b
Apps247 <67941617+Apps247@users.noreply.github.com>
2024-07-30 21:42:47 +0000

    Fixed Profile Picture Bug

--- skillsync-client/src/components/ProfilePage/EditProfilePicture.tsx
+++ skillsync-client/src/components/ProfilePage/EditProfilePicture.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { GetAvatar, SetAvatar } from "../../supabase/ProfilePicture";
-import { FaEdit, FaHourglass } from "react-icons/fa";
+import { FaEdit, FaHourglass, FaUser } from "react-icons/fa";
 import { FaCheck } from "react-icons/fa6";
 
 function EditProfilePicture() {
@@ -34,45 +34,47 @@ function EditProfilePicture() {
 
   return (
     <div className="rounded-md flex flex-col items-center justify-center">
-      {avatarURL !== "" ? (
-        <div className="relative">
+      <div className="relative">
+        {avatarURL !== "" || newAvatarURL ? (
           <img
             className="rounded-full"
             width={"200px"}
             src={newAvatarURL ? newAvatarURL : avatarURL}
           />
-          <div className="absolute right-1 bottom-1">
-            <div className="flex space-x-2">
-              <label htmlFor="single" className="bg-green-500 rounded-full">
-                <div className="cursor-pointer p-3 [bg-#1e1e1e]">
-                  <FaEdit />
-                </div>
-              </label>
-              <input
-                style={{ visibility: "hidden", position: "absolute" }}
-                type="file"
-                id="single"
-                accept="image/*"
-                onChange={handleImageUpdate}
-              />
-              {newAvatar && (
-                <button
-                  disabled={isLoading}
-                  id="submit"
-                  className="!p-3 rounded-full bg-green-500"
-                  onClick={updateProfile}
-                >
-                  {isLoading ? <FaHourglass /> : <FaCheck />}
-                </button>
-              )}
+        ) : (
+          <div className="w-[200px] h-[200px]">
+            <div className="rounded-full bg-gray-800 w-full h-full flex items-center justify-center">
+              <FaUser className="text-white w-1/2 h-1/2 m-auto" />
             </div>
           </div>
+        )}
+        <div className="absolute right-1 bottom-1">
+          <div className="flex space-x-2">
+            <label htmlFor="single" className="bg-green-500 rounded-full">
+              <div className="cursor-pointer p-3 [bg-#1e1e1e]">
+                <FaEdit />
+              </div>
+            </label>
+            <input
+              style={{ visibility: "hidden", position: "absolute" }}
+              type="file"
+              id="single"
+              accept="image/*"
+              onChange={handleImageUpdate}
+            />
+            {newAvatar && (
+              <button
+                disabled={isLoading}
+                id="submit"
+                className="!p-3 rounded-full bg-green-500"
+                onClick={updateProfile}
+              >
+                {isLoading ? <FaHourglass /> : <FaCheck />}
+              </button>
+            )}
+          </div>
         </div>
-      ) : (
-        <div className="w-[200px] h-[200px]">
-          <div className="rounded-full bg-gray-800 w-full h-full"></div>
-        </div>
-      )}
+      </div>
     </div>
   );
 }
