c22701041c7f35bfcb4a4722d5a1932fc9a6308d
Arman D <adrismir@gmail.com>
2024-09-22 16:23:03 -0700

    Competent resume download styling

--- skillsync-client/src/components/ResumeBuilder/PreviewResume.tsx
+++ skillsync-client/src/components/ResumeBuilder/PreviewResume.tsx
@@ -3,10 +3,13 @@
  * Preview resume will dynamically scale to with width of the parent.
  * This is important for nice previews, so its the parents responsibility
  * to set the width and height and to specify a height overflow setting.
+ *
+ * This component also exports a function getResumtHTML that returns the HTML of the resume.
  */
 
 import React, { useState, useEffect, useRef, useImperativeHandle, forwardRef } from "react";
 import { Resume } from "../../types/types";
+import ReactDOM from "react-dom";
 
 export type PreviewResumeRef = {
 	getResumeHTML: () => string|undefined;
@@ -21,18 +24,6 @@ const PreviewResume = forwardRef<PreviewResumeRef, Resume>(function(props, ref) 
 	useImperativeHandle(ref, () => ({
 		getResumeHTML: () => {return resumeRef.current?.outerHTML},
 	}));
-
-	// Properly formats the nested arrays in a highlights section :D
-	interface NestedStringArrayProps { highlights:string[]; };
-	function DisplayHighlights({ highlights }:NestedStringArrayProps):React.JSX.Element
-	{
-		let jsx_points:React.JSX.Element[] = [];
-		for (const hi of highlights)
-		{
-			jsx_points.push(<li key={jsx_points.length} className="">{hi}</li>);
-		}
-		return <ul className="list-disc list-outside ml-[0.30in]">{jsx_points}</ul>;
-	}
 
 	useEffect(() => {
 		function handleResize() {
@@ -61,20 +52,23 @@ const PreviewResume = forwardRef<PreviewResumeRef, Resume>(function(props, ref) 
 		backgroundColor: 'white',
 		textAlign: 'left',
 		color: 'black',
-		fontFamily: 'ui-serif,Georgia,Cambria,Times New Roman,Times,serif',
-		fontSize: '12px',
+		fontFamily: 'Times New Roman,Times,Georgia,Cambria,serif',
+		fontSize: '14px',
 		overflowY: 'hidden',
-		transformOrigin: 'top left'
+		transformOrigin: 'top left',
+		lineHeight: '20px',
 	};
 
 	const headerStyle = {
 		textAlign: 'center',
 		fontSize: '28px',
-		fontWeight: '600'
+		fontWeight: '600',
+		marginBottom: '0.05in'
 	};
 
 	const linkStyle = {
-		textDecoration: 'underline'
+		textDecoration: 'underline',
+		color: 'black',
 	};
 
 	const sectionHeader = {
@@ -86,6 +80,24 @@ const PreviewResume = forwardRef<PreviewResumeRef, Resume>(function(props, ref) 
 	const sectionElementHeader = {
 		display: 'flex',
 		justifyContent: 'space-between',
+	}
+
+	const bulletPoint = {
+		listStyleType: 'disc',
+		listStylePosition: 'outside',
+		marginLeft: '0.30in'
+	}
+
+	// Properly formats the nested arrays in a highlights section :D
+	interface NestedStringArrayProps { highlights:string[]; };
+	function DisplayHighlights({ highlights }:NestedStringArrayProps):React.JSX.Element
+	{
+		let jsx_points:React.JSX.Element[] = [];
+		for (const hi of highlights)
+		{
+			jsx_points.push(<li key={jsx_points.length} >{hi}</li>);
+		}
+		return <ul style={bulletPoint}>{jsx_points}</ul>;
 	}
 
 	return (
@@ -102,27 +114,27 @@ const PreviewResume = forwardRef<PreviewResumeRef, Resume>(function(props, ref) 
 			<div ref={resumeRef} id="ResumePreview" style={resumePreviewStyle}>
 				<h1 style={headerStyle}>{full_name}</h1>
 				<h4 style={{ textAlign: 'center' }}>
-					<span style={linkStyle}>{phone_number}</span> | 
-					<a style={linkStyle} href={`mailto:${email}`} >{email}</a> | 
-					<a style={linkStyle} href={personal_website} target="_blank">Portfolio</a> | 
-					<a style={linkStyle} href={linkedin} target="_blank">LinkedIn</a> | 
+					<span style={linkStyle}>{phone_number}</span>&nbsp;&nbsp;&nbsp;
+					<a style={linkStyle} href={`mailto:${email}`} >{email}</a>&nbsp;&nbsp;&nbsp;
+					<a style={linkStyle} href={personal_website} target="_blank">Portfolio</a>&nbsp;&nbsp;&nbsp;
+					<a style={linkStyle} href={linkedin} target="_blank">LinkedIn</a>&nbsp;&nbsp;&nbsp;
 					<a style={linkStyle} href={github} target="_blank">GitHub</a>
 				</h4>
 				<h2 style={sectionHeader}>EDUCATION</h2>
 				{education.map((ed, index:number) => (
-					<div key={index} className="ml-[0.15in]">
+					<div key={index} style={{marginLeft: '0.15in'}} >
 						<span style={sectionElementHeader}>
 							<p style={{ fontWeight: '600'}}>{ed.institution}</p><p>{ed.location}</p>
 						</span>
 						<span style={sectionElementHeader}>
-							<p className="italic font-light">{ed.degree}</p><p>{ed.end_date}</p>
+							<p style={{fontStyle: 'italic', fontWeight: 'light'}}>{ed.degree}</p><p>{ed.end_date}</p>
 						</span>
 						<DisplayHighlights highlights={ed.highlights} />
 					</div>
 				))}
 				<h2 style={sectionHeader} >EXPERIENCE</h2>
 				{experience.map( (ex, index:number) => (
-					<div key={index} className="ml-[0.15in]">
+					<div key={index} style={{marginLeft: '0.15in'}}>
 						<span style={sectionElementHeader}>
 							<p style={{ fontWeight: '600'}}>{ex.job_title}</p><p>{ex.start_day} &ndash; {ex.end_day}</p>
 						</span>
@@ -134,9 +146,9 @@ const PreviewResume = forwardRef<PreviewResumeRef, Resume>(function(props, ref) 
 				))}
 				<h2 style={sectionHeader}>PROJECTS</h2>
 				{projects.map( (pj, index:number) => (
-					<div key={index} className="ml-[0.15in]">
+					<div key={index} style={{marginLeft: '0.15in'}} >
 						<span style={sectionElementHeader}>
-							<p><a href={pj.github_url} target="_blank"><b>{pj.name}</b></a> | <i>{pj.technologies}</i></p><p>{pj.start_day} &ndash; {pj.end_day}</p>
+							<p><a style={linkStyle} href={pj.github_url} target="_blank"><b>{pj.name}</b></a> | <i>{pj.technologies}</i></p><p>{pj.start_day} &ndash; {pj.end_day}</p>
 						</span>
 						<DisplayHighlights highlights={pj.highlights} />
 					</div>
